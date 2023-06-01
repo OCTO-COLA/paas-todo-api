5262027f64d37be9500eed3b09d33046ccdcf64b
corentin.conte <corentin.conte@octo.com>
2023-06-01 12:09:46 +0200

    (feat-scalingo): un ptit move master pour du fun

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get("/", function (req, res) {
-  res.send(`<u>Hello World!</u>`)
+  res.send(`Hello World!`)
 })
 
 const port = process.env.PORT
